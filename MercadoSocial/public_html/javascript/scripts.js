a03798b2c1b038d59f037e137602a0bcc1032a8b
Renan Nascimento <renan@costanascimento.com.br>
2015-05-24 15:42:28 -0300

    Adição de alguns métodos Javascript e acertos no iframes.

--- MercadoSocial/public_html/javascript/scripts.js
+++ MercadoSocial/public_html/javascript/scripts.js
@@ -16,3 +16,53 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301  USA
  */
+function validaEmail(valor) {
+    exp_reg = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+    if (exp_reg.test(valor) === false) {
+        alert("Email inválido");
+    }
+}
+
+function validaCpf(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF === "00000000000") {
+        alert("CPF inválido!");
+        return false;
+    }
+    for (i = 1; i <= 9; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    }
+    Resto = (Soma * 10) % 11;
+    if ((Resto === 10) || (Resto === 11)) {
+        Resto = 0;
+    }
+    if (Resto !== parseInt(strCPF.substring(9, 10))) {
+        alert("CPF inválido!");
+        return false;
+    }
+    Soma = 0;
+    for (i = 1; i <= 10; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    }
+    Resto = (Soma * 10) % 11;
+    if ((Resto === 10) || (Resto === 11)) {
+        Resto = 0;
+    }
+    if (Resto !== parseInt(strCPF.substring(10, 11))) {
+        alert("CPF inválido!");
+        return false;
+    }
+    return true;
+}
+
+function confirmarSenha(senha, confirmacaoSenha) {
+    if (senha !== confirmacaoSenha) {
+        alert("Senhas diferentes!");
+    }
+}
+
+function carregarPagina(pagina) {
+    window.top.location.href = pagina;
+}
